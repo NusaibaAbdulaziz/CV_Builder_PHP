6ea83949f43cf8c3d500dc8110ef720f4faced1f
Nusaiba <Nusaibaabdulaziz@outlook.com>
2022-03-14 21:30:44 +0400

    First commit

--- buildcv.php
+++ buildcv.php
@@ -1,4 +1,21 @@
 <?php
+ session_start();
+ ?>
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <meta name="descripation" content="curriculum vitae builder">
+        <meta name="viewport" content="width=device-width, intial-scale=0.1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <title>CV builder</title>
+          
+    </head>
+    <body >
+<?php
  
 include_once 'header.php';
 
@@ -20,3 +37,6 @@ include_once 'header.php';
  include_once 'footer.php';
 
  ?>
+
+</body>
+</html>
--- header.php
+++ header.php
@@ -1,20 +1,4 @@
-<?php
- session_start();
- ?>
 
-
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8"/>
-        <meta name="descripation" content="curriculum vitae builder">
-        <meta name="viewport" content="width=device-width, intial-scale=0.1">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-        <link rel="stylesheet" href="css/bootstrap.css">
-        <title>CV builder</title>
-          
-    </head>
-    <body >
         <nav class="navbar navbar-expand-lg navbar-light bg-secondary">
             <div class="container">
             <a class="navbar-brand" href="aboutus.html"><img src="logo0.png" width="140"> </a>
